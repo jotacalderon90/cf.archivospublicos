c3ca7ac8b210290eaa8deb04a58db4b4d15dfb11
jhonatan calderon <jcalderon.aprog@gmail.com>
2022-03-23 22:34:51 -0300

    mejoras en archivo helper

--- frontend/lib/helper.js
+++ frontend/lib/helper.js
@@ -68,5 +68,97 @@ document.helper = {
 		cadena = cadena.replace(/ú/gi,"u");
 		cadena = cadena.replace(/ñ/gi,"n");
 		return cadena;
+	},
+	createService: function(METHOD,URL,headerContentType){
+		METHOD = METHOD.toUpperCase();
+		
+		const URIBuild = function(uri,params){
+			for(let attr in params){
+				uri = uri.replace(":"+attr,params[attr]);
+			}
+			return uri;
+		}
+			
+		const execute = function(method,url,body,headerContentType){
+			return new Promise(function(resolve, reject) {
+				const xhttp = new XMLHttpRequest();
+				xhttp.onreadystatechange = function() {
+					if (xhttp.readyState == 4) {
+						if(xhttp.status == 401){
+							location.reload();
+						}
+						try{
+							xhttp.json = JSON.parse(xhttp.responseText);
+							if(xhttp.json.data!=null){
+								resolve(xhttp.json.data);
+							}else{
+								throw(xhttp.json.error);
+							}
+						}catch(e){
+							reject({
+								error: e,
+								xhttp: xhttp
+							});
+						}
+					}
+				};
+				xhttp.open(method,url);
+				if(body!=undefined){
+					if(headerContentType!=""){
+						xhttp.setRequestHeader("Content-Type", headerContentType);
+					}
+					if(typeof body!="string"){
+						body = JSON.stringify(body);
+					}
+					xhttp.send(body);
+				}else{
+					xhttp.send();
+				}
+			});
+		}
+		
+		if(METHOD == "GET" || METHOD == "DELETE"){
+			return function(params){
+				return execute(METHOD,URIBuild(URL,params),undefined,undefined);
+			}
+		}else if(METHOD == "POST" || METHOD == "PUT"){
+			return function(params,body){
+				return execute(METHOD,URIBuild(URL,params),body,headerContentType || 'application/json;charset=UTF-8');
+			}
+		}
+	},
+	createServices: function(baseurl){
+		const services = {
+			total:		["GET", 	baseurl + "/total?query=:query"],
+			collection: ["GET", 	baseurl + "/collection?query=:query&options=:options"],
+			tag:		["GET",		baseurl + "/tag/collection"],
+			create:		["POST",	baseurl],
+			read:		["GET", 	baseurl + "/:id"],
+			update:		["PUT",		baseurl + "/:id"],
+			delete:		["DELETE",	baseurl + "/:id"]
+		};
+		for(service in services){
+			services[service] = this.createService(services[service][0],services[service][1]);
+		}
+		return services;
+	},
+	formatBody: function(doc,headerContentType){
+		if(headerContentType){
+			const formData = new FormData();
+			formData.append("file", doc.file);
+			return formData;
+		}else{
+			return JSON.stringify(doc);
+		}
+	},
+	scrolling: function(parent,callback){
+		$(window).scroll(()=>{
+			if(Math.round($(window).scrollTop() + $(window).height()) == Math.round($(document).height())) {
+				if(parent.obtained < parent.cant && !parent.obtaining){
+					parent[callback]();
+				}
+			}
+		});	
 	}
+
 }
