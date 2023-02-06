cdf862499ed986c421c6ae33d21f232e835adcfc
jhonatan calderon <jcalderon.aprog@gmail.com>
2023-02-06 11:13:19 -0300

    avance

--- frontend/lib/trascender/background.js
+++ frontend/lib/trascender/background.js
@@ -1,25 +1,33 @@
 const background = function(parent){
 	this.service = createService('GET','/api/background/collection');
-	this.serviceWWW = createService('GET',location.protocol + '//' + location.host.replace(location.host.split('.')[0],'www') + '/api/background/collection');
 }
 
 background.prototype.start = async function(){
 	let images;
+	let ext = false;
 	try{
 		images = await this.service();
 	}catch(e){
+		//en otros host como blog se cae por 404, por ende llamar a host www
 		console.log(e);
-		images = await this.serviceWWW();
+		ext = location.protocol + '//' + location.host.replace(location.host.split('.')[0],'www');
+		this.service = createService('GET',ext + '/api/background/collection');
+		images = await this.service();
 	}
 	if(!images.data || images.data.length==0){
 		return;
 	}
+	images = images.data.map((image)=>{
+		return {image: (ext)?location.protocol + '//' + image:image};
+	});
+	this.supersized(images);
+}
+
+background.prototype.supersized = function(images){
 	jQuery(function($) {
-		$.supersized({    
-			slides: images.data.map((image)=>{
-				return {image: image};
-			})
-		});
+		$.supersized({
+			slides: images.images
+		})
 	});
 }
 
