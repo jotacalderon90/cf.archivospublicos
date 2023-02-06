12c900406c1576bd67702e22e6f09c5f2c997f8e
jhonatan calderon <jcalderon.aprog@gmail.com>
2023-02-06 10:52:33 -0300

    avance

--- frontend/lib/trascender/global.js
+++ frontend/lib/trascender/global.js
@@ -410,3 +410,51 @@ const downloadXLSX = function(data,filename) {
 	workbook.Sheets["First"] = worksheet;
 	XLSX.writeFile(workbook, filename + '.xlsx');
 }
+
+/*TOUCH*/
+const addTouchEvent = function(element,parent,callback){
+	
+	let xDown,yDown,detectSwipe;
+	
+	const getTouches = function(evt){
+		return evt.touches || // browser API
+		evt.originalEvent.touches; // jQuery
+	}
+	
+	const handleTouchStart = function(evt) {
+		const firstTouch = getTouches(evt)[0];                                      
+		xDown = firstTouch.clientX;                                      
+		yDown = firstTouch.clientY;                                      
+	}
+	
+	const handleTouchMove = function(evt) {
+		if ( ! xDown || ! yDown ) {
+			return;
+		}
+		var xUp = evt.touches[0].clientX;                                    
+		var yUp = evt.touches[0].clientY;
+		var xDiff = xDown - xUp;
+		var yDiff = yDown - yUp;
+		if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+			if ( xDiff > 0 ) {
+				detectSwipe = 'left';
+			} else {
+				detectSwipe = 'right';
+			}                       
+		} else {
+			if ( yDiff > 0 ) {
+				detectSwipe = 'up';
+			} else { 
+				detectSwipe = 'down';
+			}                                                                 
+		}
+		/* reset values */
+		xDown = null;
+		yDown = null;                             
+	}
+	
+	document.querySelector(element).addEventListener('touchstart', (evt)=>{handleTouchStart(evt)});
+	document.querySelector(element).addEventListener('touchmove', (evt)=>{handleTouchMove(evt)});
+	document.querySelector(element).addEventListener('touchend', ()=>{parent[callback](detectSwipe);detectSwipe = null;});
+		
+}
