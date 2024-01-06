c4fb7eff126c015afc8420228df558d51a81f8dc
jhonatan calderon <jcalderon.aprog@gmail.com>
2024-01-06 00:54:15 -0300

    mejora integracion vue

--- backend/00.filemanager.views.js
+++ backend/00.filemanager.views.js
@@ -6,6 +6,12 @@ module.exports = {
 	//@method(['get'])
 	index: function(req,res){
 		res.render('filemanager/page/index/_');
+	},
+		
+	//@route('/vue')
+	//@method(['get'])
+	vue: function(req,res){
+		res.render('vue/dist/index');
 	}
 	
 }
