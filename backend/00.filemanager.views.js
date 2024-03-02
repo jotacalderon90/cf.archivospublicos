c9fc3cb9949e521c7b8896c8c6b00faaabb1f6ab
Jhonatan Calderon <jhonatan.calderon@catena.cl>
2024-03-02 00:24:29 -0300

    avance con nueva version

--- backend/00.filemanager.views.js
+++ backend/00.filemanager.views.js
@@ -6,12 +6,6 @@ module.exports = {
 	//@method(['get'])
 	index: function(req,res){
 		res.render('filemanager/page/index/_');
-	},
-		
-	//@route('/vue')
-	//@method(['get'])
-	vue: function(req,res){
-		res.render('vue/dist/index');
 	}
 	
 }
