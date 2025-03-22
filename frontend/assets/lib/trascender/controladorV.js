27fdcba6ba2d3538d0b39e67933b51f6444f1111
JcalderonCatena <jhonatan.calderon@catena.cl>
2025-03-22 00:23:05 -0300

    avance

--- frontend/assets/lib/trascender/controladorV.js
+++ frontend/assets/lib/trascender/controladorV.js
@@ -19,3 +19,32 @@ Vue.createApp({
     },
     mounted() {this.mounthed()}
 }).mount("#app");
+
+/*
+Vue.createApp({
+    data() {
+        return {
+            modules: {},
+            loader: { active: false, title: '' }
+        };
+    },
+    methods: {
+        async initializeModules() {
+            this.loader.active = true;
+
+            for (const moduleName in app.modules) {
+                const moduleInstance = new app.modules[moduleName]();
+                this.modules[moduleName] = moduleInstance;
+
+                if (moduleInstance.start) {
+                    await moduleInstance.start(this);
+                }
+            }
+
+            this.loader.active = false;
+        }
+    },
+    async mounted() {
+        await this.initializeModules();
+    }
+}).mount("#app");*/
