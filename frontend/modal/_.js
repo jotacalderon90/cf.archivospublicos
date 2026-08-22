8e75998944dddb8bf77d8717c5189ea09c3b5428
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-08-21 21:48:21 -0400

    mejora modals

--- frontend/modal/_.js
+++ frontend/modal/_.js
@@ -1,6 +1,28 @@
 const modal = function() {
-  this.notifyMsg = '';
-	this.notifyType = '';
+  
+  this.data = {
+    mdCurrent: '',
+    notify: {
+      msg: '',
+      type: ''
+    },
+    confirm: {
+      msg: '',
+      okText: '',
+      cancelText: ''
+    },
+    prompt: {
+      msg: '',
+      type: 'text',
+      placeholder: '',
+      value: ''
+    },
+    html: {
+      msg: '',
+      html: ''
+    }
+  }
+  
 }
 
 modal.prototype.open = function (id) {
@@ -15,37 +37,60 @@ modal.prototype.close = function (id) {
 	bootstrap.Modal.getOrCreateInstance(el).hide();
 };
 
+modal.prototype.aceptar = function (data) {
+  this.close(this.data.mdCurrent);
+  this.resolve(data || true);
+}
+
+//usado por prompt
+modal.prototype.continuar = function (data) {
+  this.close(this.data.mdCurrent);
+  this.resolve(data);
+}
+
+modal.prototype.cancelar = function (data) {
+  this.close(this.data.mdCurrent);
+  this.resolve(data || false);
+}
+
 modal.prototype.notify = async function (msg, type) {
   return new Promise((resolve,reject)=>{    
-    this.notifyMsg  = msg;
-    this.notifyType = type || 'success';
-    this.open('mdNotify');
-    document.getElementById('btnMdAceptar').addEventListener( 'click' , (event) => {
-      this.close('mdNotify');
-      resolve(true);
-    });
+    this.resolve = resolve;
+    this.reject = reject;
+    this.data.notify.msg  = msg;
+    this.data.notify.type = type || 'success';
+    this.data.mdCurrent = 'mdNotify';
+    this.open(this.data.mdCurrent);
   });
 };
 
-modal.prototype.confirm = function (message, cancelText, okText) {
+modal.prototype.confirm = function (msg, cancelText, okText) {
   return new Promise((resolve,reject)=>{    
-    this.message  = message;
-    this.cancelText  = cancelText;
-    this.okText  = okText;
-    this.open('mdConfirm');
-    document.getElementById('btnMdConfirmOk').addEventListener( 'click' , (event) => {
-      this.close('mdConfirm');
-      resolve(true);
-    });
-    document.getElementById('btnMdConfirmCancel').addEventListener( 'click' , (event) => {
-      this.close('mdConfirm');
-      resolve(false);
-    });
+    this.resolve = resolve;
+    this.reject = reject;
+    this.data.confirm.msg = msg;
+    this.data.confirm.cancelText = cancelText;
+    this.data.confirm.okText = okText;
+    this.data.mdCurrent = 'mdConfirm';
+    this.open(this.data.mdCurrent);
   });
 };
 
-modal.prototype.displayHtml = function (title, html) {
-  this.title  = title;
+modal.prototype.prompt = function(msg, type, placeholder, value) {
+	return new Promise((resolve, reject) => {
+    this.resolve = resolve;
+    this.reject = reject;
+    this.data.prompt.msg = msg;
+    this.data.prompt.type = type;
+    this.data.prompt.placeholder = placeholder;
+    this.data.prompt.value = value || '';
+    this.data.mdCurrent = 'mdPrompt';
+    this.open(this.data.mdCurrent);
+	});
+}
+
+modal.prototype.displayHtml = function (msg, html) {
+  this.data.html.msg  = msg;
   document.getElementById('dvMdHtml').innerHTML = html;
   this.open('mdHtml');
 };
